1b7042c1019b7ef5e4d54ece3a38e14825d40947
LJJordan124 <jordan3313.lj@gmail.com>
2023-09-02 20:18:41 -0400

    server update

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const typeDefs = `
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+input BooksInput {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    createUser(username: String!, email: String!, password: String!): Auth
+
+    saveBook(newBook: BooksInput!): User
+    deleteBook(bookId: ID!): User
+
+}
+`;
+
+module.exports = typeDefs;
